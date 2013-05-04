4882a636b449acabe4001a3e70bb94dbfa45ed1b
zeref <zerefs@gmail.com>
2013-05-04 12:49:22 +0200

    Updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 if __name__=='__main__':
     setup(name='letmenotifyu',
-          version='0.1',
+          version='1.5.0',
           description='Program to notify users of new movie and series episode release from www.1channel.ch',
           author='Lunga Mthembu (zeref)',
           author_email='zerefs@gmail.com',
