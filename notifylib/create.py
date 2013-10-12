ea8d3e3389fafb1247e2428800dcd822cc9c1582
zeref <zerefs@gmail.com>
2013-10-12 13:25:23 +0200

    1.7.1

--- notifylib/create.py
+++ notifylib/create.py
@@ -10,21 +10,40 @@ def make_series_table(cursor):
 def make_episode_table(cursor):
     cursor.execute('CREATE TABLE episodes(id INTEGER PRIMARY KEY,title VARCHAR(30),episode_name VARCHAR(15), episode_link VARCHAR(40), Date TIMESTAMP, FOREIGN KEY (title) REFERENCES series(title) ON DELETE CASCADE)')
 
-def alter_tables(cursor):
-    cursor.execute('ALTER TABLE series ADD COLUMN last_update TIMESTAMP,status BOOLEAN')
-    connection.commit()
-    #function to change database if needed.
+def make_version_table(cursor):
+    cursor.execute('CREATE TABLE schema_version(version VARCHAR(6))')
+    cursor.execute('INSERT INTO schema_version(version) VALUES ("1.7.1")')
+
+
 
 def create_database(sqlite_file):
     connection = sqlite.connect(sqlite_file)
     cursor = connection.cursor()
     cursor.execute("PRAGMA foreign_keys = ON")
-    connection.commit()
     make_movie_table(cursor)
-    connection.commit()
     make_series_table(cursor)
-    connection.commit()
     make_episode_table(cursor)
     connection.commit()
     connection.close()
 
+
+def upgrade_database(sqlite_file):
+    connect= sqlite.connect(sqlite_file)
+    cursor= connect.cursor()
+    cursor.execute("SELECT version FROM schema_version")
+    row=cursor.fetchone()
+    database_version= row[0]
+    
+    if database_version=='1.7.0':
+        print("Need to upgrade to version 1.7.1")
+        cursor.execute("ALTER TABLE series ADD COLUMN last_update TIMESTAMP")
+        cursor.execute("ALTER TABLE series ADD COLUMN status BOOLEAN")
+        cursor.execute("UPDATE schema_version SET version='1.7.1' WHERE id=1")
+        connect.commit()
+        database_version="1.7.1"
+        print("Datebase Updated")
+
+        
+        
+        
+    
