814cebf3231d759ab7948f6d6c9d3ee23167da84
zeref <zerefs@gmail.com>
2014-01-08 18:36:23 +0200

    Removed Quit Item

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup
-import py2exe
 if __name__=='__main__':
     setup(name='letmenotifyu',
           version='1.8.0',
@@ -10,7 +9,7 @@ if __name__=='__main__':
           license='GPL',
           scripts=['letmenotifyu'],
           packages=['notifylib'],
-          data_files=[('share/applications',['ui/letmenotifyu.desktop']),('share/letmenotifyu',['ui/about7.glade','ui/confirm7.glade','ui/input7.glade', 'ui/main7.glade','ui/stats7.glade','ui/error.glade','ui/preferences.glade']), ('share/letmenotifyu',['ui/letmenotifyu.png','ui/letmenotifyu.xpm'])]
+          data_files=[('share/applications',['ui/letmenotifyu.desktop']),('share/letmenotifyu',['ui/about.glade','ui/confirm.glade','ui/input.glade', 'ui/main.glade','ui/stats.glade','ui/error.glade','ui/preferences.glade']), ('share/letmenotifyu',['ui/letmenotifyu.png','ui/letmenotifyu.xpm'])]
                       
           )
             
