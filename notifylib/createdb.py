7355de6015b182d00e6f4d1f5ca2d4c104e31784
zeref <zerefs@gmail.com>
2014-08-14 19:07:11 +0200

    clean

--- notifylib/createdb.py
+++ notifylib/createdb.py
@@ -101,3 +101,7 @@ class Database:
             logging.info("***Database has been upgraded***")
             logging.info("***Creating Images Directory***")
             os.mkdir(settings.IMAGE_PATH)
+            database_version = '2.0'
+
+        if database_version == '2.0':
+            logging.info("database is the latest")
--- notifylib/torrent.py
+++ notifylib/torrent.py
@@ -2,30 +2,30 @@ import logging
 import webbrowser
 
 class Torrent:
-        def __init__(self, title, cursor, connect):
-                self.title = title
-                self.cursor = cursor
-                self.connect = connect
-                self.split_title = self.title.split("-")
-                
-        def kickass(self):
-                self.cursor.execute("Select link  from torrents where name='Kickass'")
-                result = self.cursor.fetchone()
-                webbrowser.open_new(result[0]+self.split_title[0])
-                logging.info("Opening kickass Link")
-                
-        def isohunt(self):
-                self.cursor.execute("SELECT link from torrents where name='Isohunt'")
-                result = self.cursor.fetchone()
-                webbrowser.open_new(result[0]+self.split_title[0])
-                logging.info("Opening isohunt Link")
-                
-        def piratebay(self):
-                self.cursor.execute("SELECT link FROM torrents where name='The Pirate Bay")
-                result = self.cursor.fetchone()
-                webbrowser.open_new(result[0]+self.split_title[0])
-                logging.info("Opening Piratebay Link")
-                
-        def online(self, dic):
-                webbrowser.open_new(dic[self.title])
-                logging.info("Opening online Link")
+    def __init__(self, title, cursor, connect):
+        self.title = title
+        self.cursor = cursor
+        self.connect = connect
+        self.split_title = self.title.split("-")
+    
+    def kickass(self):
+            self.cursor.execute("Select link  from torrents where name='Kickass'")
+            result = self.cursor.fetchone()
+            webbrowser.open_new(result[0]+self.split_title[0])
+            logging.info("Opening kickass Link")
+
+    def isohunt(self):
+            self.cursor.execute("SELECT link from torrents where name='Isohunt'")
+            result = self.cursor.fetchone()
+            webbrowser.open_new(result[0]+self.split_title[0])
+            logging.info("Opening isohunt Link")
+
+    def piratebay(self):
+            self.cursor.execute("SELECT link FROM torrents where name='The Pirate Bay")
+            result = self.cursor.fetchone()
+            webbrowser.open_new(result[0]+self.split_title[0])
+            logging.info("Opening Piratebay Link")
+
+    def online(self, dic):
+            webbrowser.open_new(dic[self.title])
+            logging.info("Opening online Link")
