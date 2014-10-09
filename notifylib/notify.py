0475a1ab4b9bc610f1ea500f53d25a153a58e725
zeref <zerefs@gmail.com>
2014-10-09 10:30:26 +0200

    ready for merge

--- notifylib/notify.py
+++
@@ -1,8 +0,0 @@
-
-from gi.repository import Notify
-
-def announce(type_s,title,link=''):
-    Notify.init("Letmenotifyu")
-    announcement = Notify.Notification.new(type_s+'\n',title+'\n'+link,
-                                             'dialog-information')
-    announcement.show()
