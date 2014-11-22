f603da03727ce938051750349adac7323051f48e
zeref <shellcomplex7@gmail.com>
2014-11-22 11:53:16 +0200

    devel

--- notifylib/settings.py
+++ notifylib/settings.py
@@ -1,10 +1,10 @@
 import os
 
-DIRECTORY_PATH = os.environ['HOME']+'/.local/share/letmenotifyu/'
+DIRECTORY_PATH = os.getcwd()
 
-DATABASE_PATH = DIRECTORY_PATH+'dev.sqlite'
+DATABASE_PATH = DIRECTORY_PATH+'/dev.sqlite'
 
-IMAGE_PATH = DIRECTORY_PATH+'images/'
+IMAGE_PATH = DIRECTORY_PATH+'/images/'
 
-LOG_FILE_PATH = DIRECTORY_PATH+'dev.log'
+LOG_FILE_PATH = DIRECTORY_PATH+'/dev.log'
 
