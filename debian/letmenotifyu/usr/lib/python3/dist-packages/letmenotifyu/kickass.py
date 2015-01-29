fb9f2b2ffe207de0a33c4dceff1a989c1081731e
Lunga Mthembu <shellcomplex7.gmail.com>
2015-01-29 20:29:12 +0200

    merge

--- debian/letmenotifyu/usr/lib/python3/dist-packages/letmenotifyu/kickass.py
+++ debian/letmenotifyu/usr/lib/python3/dist-packages/letmenotifyu/kickass.py
@@ -7,7 +7,7 @@ def search_episode(kickass_file, title, episode_name, uploader):
     "search for episode"
     with open(kickass_file,'r') as f:
         for line in f:
-            if re.search(r'%s' %(title+".*"+episode_name+'.*'+uploader),line):
+            if re.search(r'%s' %(title+".*"+episode_name+' (?!720p).*'+uploader),line):
                 logging.debug("found {}".format(line))
                 episode_info = line.split("|")
                 return episode_info
