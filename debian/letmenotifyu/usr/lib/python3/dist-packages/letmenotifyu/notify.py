adfd51097b1333d3fa30c169b4a39f47d4a5a438
Lunga Mthembu <shellcomplex7.gmail.com>
2015-02-15 23:06:21 +0200

    debian files

---
+++ debian/letmenotifyu/usr/lib/python3/dist-packages/letmenotifyu/notify.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from gi.repository import Notify
+
+def announce(type_s, title, link=''):
+    Notify.init("Letmenotifyu")
+    announcement = Notify.Notification.new(type_s+'\n', title+'\n'+link,
+                                             'dialog-information')
+    announcement.show()
+
