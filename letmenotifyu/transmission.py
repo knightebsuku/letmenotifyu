a283bd0a74d7197fa1dffb68aba5cf109bfea7bd
Lunga Mthembu <shellcomplex7.gmail.com>
2015-09-11 19:43:24 +0200

    function to handle connecting to transmission

--- letmenotifyu/transmission.py
+++ letmenotifyu/transmission.py
@@ -7,21 +7,24 @@ from . import util
 logging.getLogger('transmissionrpc')
 
 
-def add_torrent(torrent_file_path, cursor):
-    "add torrent to transmission"
+def open_transmission(cursor):
+    "connect to transmissionrpc"
     host = util.get_config_value(cursor, 'transmission_host')
     port = util.get_config_value(cursor, 'transmission_port')
-    client = transmissionrpc.Client(host, port=str(int(port)))
+    return transmissionrpc.Client(host, port=port[:-2])
+
+
+def add_torrent(torrent_file_path, cursor):
+    "add torrent to transmission"
+    client = open_transmission(cursor)
     details = client.add_torrent(torrent_file_path)
     return details.hashString, details.name
 
 
-def check_movie_status(transmission_hash, cursor, db):
+def check_movie_status(watch_id, transmission_hash, cursor, db):
     try:
-        host = util.get_config_value(cursor, 'transmission_host')
-        port = util.get_config_value(cursor, 'transmission_port')
-        tc = transmissionrpc.Client(host, port=str(int(port)))
-        torrent_status = tc.get_torrent(transmission_hash)
+        client = open_transmission(cursor)
+        torrent_status = client.get_torrent(transmission_hash)
         cursor.execute("SELECT mq.id FROM "\
                            "movie_queue AS mq JOIN "\
                            "movie_torrent_links AS mtl ON "\
@@ -29,41 +32,41 @@ def check_movie_status(transmission_hash, cursor, db):
                            "transmission_hash=%s",
                            (transmission_hash,))
         (queue_id,) = cursor.fetchone()
-        if torrent_status.status == 'downloading':
+        if watch_id == 2 and torrent_status.status == 'downloading':
             logging.debug("updating movie queue Id {} to status 3".format(queue_id))
             cursor.execute("UPDATE movie_queue SET watch_queue_status_id=3 " \
                                       "WHERE id=%s", (queue_id,))
             db.commit()
-        elif torrent_status.isFinished:
+        elif watch_id == 3 and torrent_status.isFinished:
             logging.debug("updating movie queue Id {} to status 4".format(queue_id))
             cursor.execute("UPDATE movie_queue SET watch_queue_status_id=4 " \
                                       "WHERE id=%s", (queue_id,))
             db.commit()
+    except KeyError as e:
+        logging.warn("unable to find movie torrent for {}".format(queue_id))
     except Exception as e:
         logging.exception(e)
 
 
-def check_episode_status(queue_id, cursor, db):
+def check_episode_status(watch_id, queue_id, cursor, db):
     "check status of episode download"
     try:
         cursor.execute("SELECT transmission_hash FROM series_torrent_links "\
                        "WHERE series_queue_id=%s", (queue_id,))
         (transmission_hash,) = cursor.fetchone()
-        host = util.get_config_value(cursor, 'transmission_host')
-        port = util.get_config_value(cursor, 'transmission_port')
-        tc = transmissionrpc.Client(host, port=str(int(port)))
-        torrent_status = tc.get_torrent(transmission_hash)
-        if torrent_status.status == 'downloading':
+        client = open_transmission(cursor)
+        torrent_status = client.get_torrent(transmission_hash)
+        if watch_id == 2 and torrent_status.status == 'downloading':
             logging.debug("updating series queue Id {} to status 3".format(queue_id))
             cursor.execute("UPDATE series_queue SET watch_queue_status_id=3 "\
                                           "WHERE id=%s", (queue_id,))
             db.commit()
-        elif torrent_status.isFinished:
+        elif watch_id == 3 and torrent_status.isFinished:
             logging.debug("updating series queue Id {} to status 4".format(queue_id))
             cursor.execute("UPDATE series_queue SET watch_queue_status_id=4 "\
                                           "WHERE id=%s", (queue_id,))
             db.commit()
     except KeyError as e:
-        logging.warn("unable to find torrent for {}".format(queue_id))
+        logging.warn("unable to find series torrent for {}".format(queue_id))
     except Exception as e:
         logging.exception(e)
