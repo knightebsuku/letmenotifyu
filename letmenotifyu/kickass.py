7418862b6d4de066586e72faebe411777eb141dd
Lunga Mthembu <shellcomplex7.gmail.com>
2015-09-21 01:12:09 +0200

    change from torrent link to magnet link

--- letmenotifyu/kickass.py
+++ letmenotifyu/kickass.py
@@ -22,10 +22,14 @@ def fetch_episode_search_results(series_name, episode_number):
             result_title = results.find('a', 'cellMainLink').text
             if re.search(r'{title} {episode_number} HDTV x264-(LOL|KILLERS|ASAP|2HD|FUM|TLA|BATV)'.format(title=series_name, episode_number=episode_number), result_title):
                 for urls in results.find_all('a', 'icon16'):
-                    if urls.get('title') == 'Download torrent file':
-                        logging.debug("found torrent link for {}-{}".format(series_name, episode_number))
+                    if urls.get('title') == 'Torrent magnet link':
+                        logging.debug("found magnet link for {}-{}".format(series_name, episode_number))
                         logging.debug(urls.get('href'))
                         return urls.get('href')
+                    #if urls.get('title') == 'Download torrent file':
+                       # logging.debug("found torrent link for {}-{}".format(series_name, episode_number))
+                        #logging.debug(urls.get('href').split('?')[0])
+                        #return urls.get('href').split('?')[0]
     except requests.exceptions.ConnectionError as e:
         logging.debug("unable to get torrent url for {}-{}".format(series_name, episode_number))
         logging.exception(e)
