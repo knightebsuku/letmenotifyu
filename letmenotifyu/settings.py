a1ea89d3dfffbdfcb67f8178695ab6172b635670
lunga <lunga@complex-desktop>
2016-11-22 07:37:39 +0200

    changes for new config file and database creation

--- letmenotifyu/settings.py
+++ letmenotifyu/settings.py
@@ -5,13 +5,15 @@ import configparser
 import logging
 import psycopg2
 
+log = logging.getLogger(__name__)
+
 # DIRECTORY_PATH = os.path.join(os.environ['HOME'], '.letmenotifyu')
 DIRECTORY_PATH = 'DevLetme'
-DATABSE_PATH = 'database'
+DATABSE_PATH = os.path.join(DIRECTORY_PATH, 'database')
 MOVIE_DB = os.path.join(DATABSE_PATH, 'movie.db')
 SERIES_DB = os.path.join(DATABSE_PATH, 'series.db')
 LOG_FILE_PATH = os.path.join(DIRECTORY_PATH, 'letmenotifyu.log')
-DATA_FILES_PATH = '/usr/share/letmenotifyu/'
+#DATA_FILES_PATH = '/usr/share/letmenotifyu/'
 IMAGE_PATH = os.path.join(DIRECTORY_PATH, 'images')
 config = configparser.ConfigParser()
 
@@ -23,61 +25,35 @@ def logging_dict(log_level):
 
 
 def create_ini_file():
-    complete_directory = os.path.join(DIRECTORY_PATH, 'complete')
-    incomplete_directory = os.path.join(DIRECTORY_PATH, 'incomplete')
-    
-    config['DIRECTORIES'] = {
-                             'CompleteDownloads': complete_directory,
-                             'IncompleteDownloads': incomplete_directory}
-    config['DATABASE'] = {'Host': '172.16.210.128',
-                          'Port': '5432',
-                          'User': 'letmenotifyu',
-                          'Password': 'letmenotifyu',
-                          'Database': 'letmenotifyu'}
+    """
+    Create config file upon install start of application
+    """
+    config['DIRECTORIES'] = {'CompleteDownloads': os.path.join(DIRECTORY_PATH, 'complete'),
+                             'IncompleteDownloads': os.path.join(DIRECTORY_PATH, 'incomplete')}
     config["LOGGING"] = {'LoggingLevel': "Logging.INFO"}
     with open(DIRECTORY_PATH+'/config.ini', 'w') as cfg_file:
         config.write(cfg_file)
 
 
 def check_db():
-    try:
-        connect = psycopg2.connect(host=DB_HOST,
-                                   database=DB_NAME,
-                                   port=DB_PORT,
-                                   user=DB_USER,
-                                   password=DB_PASSWORD)
-        try:
-            cursor = connect.cursor()
-            cursor.execute("SELECT max(id) from migration")
-        except Exception as e:
-            logging.exception(e)
-            return 'migration'
-    except psycopg2.OperationalError as e:
-        logging.exception(e)
-        return 'connect'
-
+    if os.path.exists(MOVIE_DB) and os.path.exists(SERIES_DB):
+        return True
+    else:
+        log.error("movie or series database do not exist")
+        raise ValueError("movie or series database not found")
 
 try:
     config.read(DIRECTORY_PATH+'/config.ini')
     COMPLETE_DIRECTORY = config['DIRECTORIES']['CompleteDownloads']
     INCOMPLETE_DIRECTORY = config['DIRECTORIES']['IncompleteDownloads']
     LOG_LEVEL = logging_dict(config['LOGGING']['LoggingLevel'])
-    DB_NAME = config['DATABASE']['Database']
-    DB_HOST = config['DATABASE']['Host']
-    DB_PORT = config['DATABASE']['Port']
-    DB_USER = config['DATABASE']['User']
-    DB_PASSWORD = config['DATABASE']['Password']
 except KeyError:
     os.mkdir(DIRECTORY_PATH)
+    os.mkdir(IMAGE_PATH)
+    os.mkdir(DATABSE_PATH)
     create_ini_file()
     COMPLETE_DIRECTORY = config['DIRECTORIES']['CompleteDownloads']
     INCOMPLETE_DIRECTORY = config['DIRECTORIES']['IncompleteDownloads']
     LOG_LEVEL = logging_dict(config['LOGGING']['LoggingLevel'])
-    DB_NAME = config['DATABASE']['Database']
-    DB_HOST = config['DATABASE']['Host']
-    DB_PORT = config['DATABASE']['Port']
-    DB_USER = config['DATABASE']['User']
-    DB_PASSWORD = config['DATABASE']['Password']
-    os.mkdir(IMAGE_PATH)
     os.mkdir(INCOMPLETE_DIRECTORY)
     os.mkdir(COMPLETE_DIRECTORY)
