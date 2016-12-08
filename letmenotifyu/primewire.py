280d15fdd29c8fa36003ec760bab18c4c7ed7369
lunga <lunga@localhost>
2016-12-08 11:30:26 +0200

    return json from primewire

--- letmenotifyu/primewire.py
+++ letmenotifyu/primewire.py
@@ -3,24 +3,39 @@
 import requests
 import logging
 import re
+import json
 
 from bs4 import BeautifulSoup
+from requests.exceptions import ConnectionError, HTTPError
+from typing import Dict
+
+Json = Dict[str, str]
+
+log = logging.getLogger(__name__)
 
 
-def check_episode_numbers(episode_number):
+def series_details(series_url: str) -> BeautifulSoup:
+    try:
+        response = requests.get(series_url)
+        return BeautifulSoup(response.text)
+    except (ConnectionError, HTTPError):
+        raise
+
+
+def check_episode_numbers(episode_number: str) -> bool:
     "check if episode numbers are valid"
     if 0 < int(episode_number) <= 26:
         return True
 
 
-def check_season_number(season_number):
+def check_season_number(season_number: str) -> bool:
     "check if season number is valid"
     if int(season_number) > 0:
         return True
 
 
-def modify_episode_number(season_value, episode_value):
-    "format episode number"
+def modify_episode_number(season_value: str, episode_value: str) -> str:
+    "format episode number to eg S01E02"
     if int(season_value) < 10:
         season_value = "0{}".format(season_value)
     if int(episode_value) < 10:
@@ -28,21 +43,31 @@ def modify_episode_number(season_value, episode_value):
     return''.join(('S', season_value, 'E', episode_value))
 
 
-def primewire(episode_site):
-    "process series html page"
+def primewire(series_url: str) -> Json:
     try:
-        series_page = requests.get(episode_site)
-        series_page_data = BeautifulSoup(series_page.text, "lxml")
-        all_series_info = []
-        for episode_item in series_page_data.find_all('div', {'class': 'tv_episode_item'}):
-            link = episode_item.a['href']
-            link_data = re.search(r'season-(\d+)-episode-(\d+)',link)
+        series_episodes = []
+        series_info = {}
+        series_html = series_details(series_url)
+        total_seasons = len(series_html.find_all('a',
+                                                 {'class': 'season-toggle'}))
+        total_episodes = len(series_html.find_all('div',
+                                                  {'class': 'tv_episode_item'}))
+        series_info['total_seasons'] = total_seasons
+        series_info['total_episodes'] = total_episodes
+        for episode in series_html.find_all('div', {'class': 'tv_episode_item'}):
+            link = episode.a['href']
+            link_data = re.search(r'season-(\d+)-episode-(\d+)', link)
             if check_season_number(link_data.group(1)) and check_episode_numbers(link_data.group(2)):
                 episode_number = modify_episode_number(link_data.group(1), link_data.group(2))
-                ep_name = episode_item.find('a').contents[1].text
-                all_series_info.append((link, episode_number, ep_name))
-        seasons = series_page_data.find_all('a', {'class': 'season-toggle'})
-        return all_series_info, len(all_series_info), len(seasons)
-    except Exception as e:
-        logging.warn("Unable to connect to {} ".format(episode_site))
-        logging.exception(e)
+                episode_name = episode.find('a').contents[1].text
+                episode_dict = {
+                    'episode_link': link,
+                    'episode_number': episode_number,
+                    'episode_name': episode_name
+                }
+                series_episodes.append(episode_dict)
+        series_info['episodes'] = series_episodes
+        return json.dumps(series_info)
+    except (ConnectionError, HTTPError) as error:
+        log.error("Unable to connect to primewire")
+        log.exception(error)
