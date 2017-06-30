2636fd2f840aecc7606b8e53e82ded4113e4a444
lunga <lunga@localhost>
2017-06-30 17:32:12 +0200

    fix UnboundLocalError

--- letmenotifyu/transmission.py
+++ letmenotifyu/transmission.py
@@ -58,7 +58,7 @@ def check_movie_status(watch_id, transmission_hash, cursor, db):
                            "WHERE id=?", (queue_id,))
             db.commit()
     except KeyError as e:
-        log.warn("unable to find movie torrent for {}".format(queue_id))
+        log.warn("unable to find movie torrent")
     except transmissionrpc.error.TransmissionError:
         log.error("unable to connect to transmissionrpc")
     except Exception as e:
